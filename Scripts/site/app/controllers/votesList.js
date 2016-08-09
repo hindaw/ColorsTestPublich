53059212b9e8472adb9ff492c5bff81e3f028d88
hindaw <hinda.weissbecker@gmail.com>
2016-08-10 01:03:07 +0300

    publich

---
+++ Scripts/site/app/controllers/votesList.js
@@ -0,0 +1,31 @@
+﻿function colorList($scope, $http) {
+
+    $http.get('/api/Data').success(function (list) {
+        $scope.colors = list;
+    });
+
+
+
+    $scope.getVotesByColor = function (index) {
+
+        $http.get('/api/Data?colorId=' + $scope.colors[index].Id).success(function (res) {
+            $scope.colors[index].sum = res;
+
+        })
+    };
+
+
+    $scope.getTotal = function () {
+        var t = 0
+        angular.forEach($scope.colors, function (value) {
+            if (value.sum != null)
+                t = t + value.sum;
+        });
+        $scope.total = t
+    }
+
+
+
+
+
+}
